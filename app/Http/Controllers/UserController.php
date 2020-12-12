19b59ff95a4905bd276252f5c205c77fcfaf9922
曾昭文 <wenzi2020@yeah.net>
2020-12-12 16:06:33 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public static function shopcart(){
+        return view('shopcart');
+    }
     public static function account(){
         return view('account');
     }
